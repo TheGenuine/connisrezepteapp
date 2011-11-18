a6dfc86327cc22409d83e2a41305ed6f97cf203b
Rene <reneruck@googlemail.com>
2011-11-18 01:05:23 +0100

    fixed typo in db creation moved sql to static

--- src/de/reneruck/connisRezepteApp/DBManager.java
+++ src/de/reneruck/connisRezepteApp/DBManager.java
@@ -1,13 +1,17 @@
 package de.reneruck.connisRezepteApp;
 
 import android.content.Context;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteDatabase.CursorFactory;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class DBManager extends SQLiteOpenHelper {
 
-	
+	private static final String CREATE_SQL = "create table " + Configurations.table_Rezepte + " (" + Configurations.rezept_Id + " integer primary key autoincrement, "
+			+ Configurations.rezept_Name+ " text not null, "
+			+ Configurations.rezept_Document + " text not null, " 
+			+ Configurations.rezepte_stichwoerter + " text);"; 
 	
 	public DBManager(Context context, String name, CursorFactory factory, int version) {
 		super(context, Configurations.databaseName, factory,  Configurations.databaseVersion);
@@ -19,21 +23,21 @@ public class DBManager extends SQLiteOpenHelper {
 	 *  - [int] RezeptId <primaryKey>
 	 *  - [String] RezeptName (z.B. Mein tolles Rezept)
 	 *  - [String] Document-name (z.B. Mein-Rezept.doc, ...)
-	 *  - [String] Stichwörter (z.B. Karotte, Spargel, ...)
+	 *  - [String] Stichwï¿½Ã¶rter (z.B. Karotte, Spargel, ...)
 	 *  
 	 */
 	@Override
 	public void onCreate(SQLiteDatabase db) {
-		
-		db.execSQL("create table " + Configurations.table_Rezepte + " (" + Configurations.rezept_Id + " integer primary key autoincrement, "
-				+ Configurations.rezept_Name+ "test not null, "
-				+ Configurations.rezepteDirPath + "text not null, " 
-				+ Configurations.rezepte_stichwörter + " text not null)");
+		try {
+			db.execSQL(CREATE_SQL);
+		} catch (SQLException e) {
+			System.err.println(e);
+		}
 	}
 
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int arg1, int arg2) {
-		db.execSQL("DROP TABLE " +Configurations.table_Rezepte + "");
+		db.execSQL("DROP TABLE IF EXISTS" +Configurations.table_Rezepte + "");
 		onCreate(db);
 	}
 
