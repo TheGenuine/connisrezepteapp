7615c5c605ed8343829bc911619707a0612326ef
Rene <reneruck@googlemail.com>
2011-11-18 01:04:33 +0100

    set charset to utf-8

--- src/de/reneruck/connisRezepteApp/Configurations.java
+++ src/de/reneruck/connisRezepteApp/Configurations.java
@@ -6,12 +6,12 @@ public class Configurations {
 
 	public static final  String databaseName = "rezepte.db";
 	public static final int databaseVersion = 1;
-	public static final String rezepteDirPath = Environment.getExternalStorageDirectory() + "/Rezepte/";
+	public static final String rezepteDirPath = Environment.getExternalStorageDirectory() +"/Rezepte/";
 	
 	public static String rezept_Id = "rezeptID";
 	public static String rezept_Name = "rezeptName";
 	public static String rezept_Document = "document";
-	public static String rezepte_stichwörter = "keywords";
+	public static String rezepte_stichwoerter = "keywords";
 	
 	public static final  String table_Rezepte = "rezepte";
 }
