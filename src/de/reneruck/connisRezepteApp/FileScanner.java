d65c731fcf79ad1a99b1dd940c3765ca128d78de
Rene <reneruck@googlemail.com>
2011-11-18 21:48:59 +0100

    refactored and cleaned up the filescanner

--- src/de/reneruck/connisRezepteApp/FileScanner.java
+++ src/de/reneruck/connisRezepteApp/FileScanner.java
@@ -2,6 +2,7 @@ package de.reneruck.connisRezepteApp;
 
 import java.io.File;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 import org.apache.commons.collections.ListUtils;
@@ -14,8 +15,8 @@ import android.util.Log;
 
 public class FileScanner extends AsyncTask<String, Integer, List<String>>{
 
-	private String[] documentsOnStorage;
-	private String[] documentsInDatabase;
+	private List<String> documentsOnStorage;
+	private List<String> documentsInDatabase = new LinkedList<String>();
 	private List<String> diff;
 	private NewDocumentsBean newDocumentBean;
 	
@@ -31,27 +32,29 @@ public class FileScanner extends AsyncTask<String, Integer, List<String>>{
 				
 		File rezepteDictionary = new File(Configurations.rezepteDirPath);
 		
+		// check if the directory is existent
+		// without it, there woun't be any rezepte
 		if(rezepteDictionary.exists() && rezepteDictionary.isDirectory()){
-			this.documentsOnStorage = rezepteDictionary.list();
+			
+			this.documentsOnStorage = Arrays.asList(rezepteDictionary.list());
+			
 			try {
 				Cursor documentsCursor = db.query(Configurations.table_Rezepte, new String[]{Configurations.rezept_Name}, null, null, null, null, null);
 				
+				// there is something in the saved in the database
 				if(documentsCursor.getCount() > 0){
-					this.documentsInDatabase = new String[documentsCursor.getCount()];
-					int count = 0;
-					documentsCursor.moveToFirst();
-					do {
-						int docIndex = documentsCursor.getColumnIndex(Configurations.rezept_Name);
-						this.documentsInDatabase[count] = documentsCursor.getString(docIndex);
-						count ++;
-					} while (documentsCursor.moveToNext());
 					
-					// hinzufügen = inDB - onDisc
-					this.diff = ListUtils.subtract(Arrays.asList(this.documentsOnStorage), Arrays.asList(this.documentsInDatabase));
+					// copy all entries from the cursor to a compareable list
+					for(documentsCursor.moveToFirst(); documentsCursor.moveToNext(); documentsCursor.isAfterLast()) {
+						this.documentsInDatabase.add(documentsCursor.getString(0));
+					}
+					
+					// hinzufügen = onDisc - inDB
+					this.diff = ListUtils.subtract(this.documentsOnStorage, this.documentsInDatabase);
 					this.newDocumentBean.putAllEntries(diff);
 					
 				}else{
-					this.newDocumentBean.putAllEntries(Arrays.asList(this.documentsOnStorage));
+					this.newDocumentBean.putAllEntries(this.documentsOnStorage);
 				}
 				
 			} catch (SQLException e) {
