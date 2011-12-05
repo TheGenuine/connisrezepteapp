165ce7fa3889c88d35483877640e28352b767d18
Rene <reneruck@googlemail.com>
2011-12-05 23:14:20 +0100

    ADD new databases added rezept to zutat added rezept to kategorie

--- src/de/reneruck/connisRezepteApp/Configurations.java
+++ src/de/reneruck/connisRezepteApp/Configurations.java
@@ -9,10 +9,11 @@ public class Configurations {
 	public static final String dirPath = Environment.getExternalStorageDirectory() +"/Rezepte/";
 	
 	// available SQL Tables
-	public static final String table_Keywords = "Keywords";
 	public static final String table_Rezepte = "Rezepte";
 	public static final String table_Zutaten = "Zutaten";
 	public static final String table_Kategorien = "Kategorien";
+	public static final String table_Rezept_to_Kategorie = "Rezept-Kategorie";
+	public static final String table_Rezept_to_Zutat = "Rezept-Zutat";
 	
 	// collums of Rezepte Table
 	public static String rezepte_Id = "ID";
@@ -20,12 +21,8 @@ public class Configurations {
 	public static String rezepte_DocumentName = "documentName";
 	public static String rezepte_DocumentHash = "hash";
 	public static String rezepte_PathToDocument = "documentPath";	
-	public static String rezepte_Stichwoerter = "keywords";
+	public static String rezepte_Zubereitung = "zubereitung";
 	
-	// Collums of Keywords Table
-	public static String keywords_Id = "ID";
-	public static String keywords_keyword = "keyword";
-
 	// Collums of Zutaten
 	public static String zutaten_Id = "ID";
 	public static String zutaten_value = "value";
@@ -33,4 +30,13 @@ public class Configurations {
 	// Collums of Kategorien
 	public static String kategorien_Id = "ID";
 	public static String kategorien_value = "value";
+
+	// Collums of Rezepte-Kategorie
+	public static String rezept_to_kategorie_rezeptId= "rezeptId";
+	public static String rezept_to_kategorie_kategorieId= "katId";
+	
+	// Collums of Rezepte-Zutat
+	public static String rezept_to_zutat_rezeptId= "rezeptId";
+	public static String rezept_to_zutat_zutatId= "zutatId";
+	
 }
--- src/de/reneruck/connisRezepteApp/DBManager.java
+++ src/de/reneruck/connisRezepteApp/DBManager.java
@@ -10,18 +10,22 @@ public class DBManager extends SQLiteOpenHelper {
 
 	private static final String CREATE_REZEPTE_DB = "create table " + Configurations.table_Rezepte + " (" + Configurations.rezepte_Id + " integer primary key autoincrement, "
 			+ Configurations.rezepte_Name+ " text not null, "
-			+ Configurations.rezepte_DocumentName + " text not null, " 
+			+ Configurations.rezepte_DocumentName + " text not null, "
+			+ Configurations.rezepte_DocumentHash + " integer, "
 			+ Configurations.rezepte_PathToDocument + " text , " 
-			+ Configurations.rezepte_Stichwoerter + " text);"; 
-	
-	private static final String CREATE_KEYWODS_DB = "create table " + Configurations.table_Keywords + " (" + Configurations.keywords_Id + " integer primary key autoincrement, "
-			+ Configurations.keywords_keyword + " text not null);";
+			+ Configurations.rezepte_Zubereitung + " text);"; 
 	
 	private static final String CREATE_ZUTATEN_DB = "create table " + Configurations.table_Zutaten+ " (" + Configurations.zutaten_Id + " integer primary key autoincrement, "
 			+ Configurations.zutaten_value + " text not null);";
 	
-	private static final String CREATE_KATEGORIEN_DB = "create table " + Configurations.table_Keywords + " (" + Configurations.kategorien_Id + " integer primary key autoincrement, "
+	private static final String CREATE_KATEGORIEN_DB = "create table " + Configurations.table_Kategorien+ " (" + Configurations.kategorien_Id + " integer primary key autoincrement, "
 			+ Configurations.kategorien_value + " text not null);"; 
+
+	private static final String CREATE_REZEPT_TO_ZUTATE_DB = "create table " + Configurations.table_Rezept_to_Zutat + " (" + Configurations.rezept_to_zutat_rezeptId+ " integer primary key autoincrement, "
+			+ Configurations.rezept_to_zutat_zutatId + " text not null);"; 
+	
+	private static final String CREATE_REZEPT_TO_KATEGORIE_DB = "create table " + Configurations.table_Rezept_to_Kategorie + " (" + Configurations.rezept_to_kategorie_rezeptId + " integer primary key autoincrement, "
+			+ Configurations.rezept_to_kategorie_kategorieId + " text not null);"; 
 	
 	public DBManager(Context context, String name, CursorFactory factory, int version) {
 		super(context, Configurations.databaseName, factory,  Configurations.databaseVersion);
@@ -40,9 +44,10 @@ public class DBManager extends SQLiteOpenHelper {
 	public void onCreate(SQLiteDatabase db) {
 		try {
 			db.execSQL(CREATE_REZEPTE_DB);
-			db.execSQL(CREATE_KEYWODS_DB);
 			db.execSQL(CREATE_ZUTATEN_DB);
 			db.execSQL(CREATE_KATEGORIEN_DB);
+			db.execSQL(CREATE_REZEPT_TO_ZUTATE_DB);
+			db.execSQL(CREATE_REZEPT_TO_KATEGORIE_DB);
 		} catch (SQLException e) {
 			System.err.println(e);
 		}
@@ -54,9 +59,10 @@ public class DBManager extends SQLiteOpenHelper {
 		 * TODO: Here should happen a data migration!!  
 		 */
 		db.execSQL("DROP TABLE IF EXISTS " +Configurations.table_Rezepte + "");
-		db.execSQL("DROP TABLE IF EXISTS " +Configurations.table_Keywords + "");
 		db.execSQL("DROP TABLE IF EXISTS " +Configurations.table_Zutaten + "");
 		db.execSQL("DROP TABLE IF EXISTS " +Configurations.table_Kategorien + "");
+		db.execSQL("DROP TABLE IF EXISTS " +Configurations.table_Rezept_to_Kategorie+ "");
+		db.execSQL("DROP TABLE IF EXISTS " +Configurations.table_Rezept_to_Zutat + "");
 		onCreate(db);
 	}
 
