c5978f8f50e9b3e3b9da275b2e038b2b4763ad1d
Rene <reneruck@googlemail.com>
2011-12-10 19:28:48 +0100

    ADD Application extension class atm nothing to do, but its good to have one :-)

---
+++ src/de/reneruck/connisRezepteApp/AppContext.java
@@ -0,0 +1,11 @@
+package de.reneruck.connisRezepteApp;
+
+import android.app.Application;
+
+public class AppContext extends Application {
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+	}
+}
