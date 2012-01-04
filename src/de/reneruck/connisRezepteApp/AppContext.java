5e28aa1b02ea704300ac5baa59acdf2c3f4c6754
Rene <reneruck@googlemail.com>
2012-01-04 23:38:03 +0100

    INDEVELOPMENT changes in sql statement

--- src/de/reneruck/connisRezepteApp/AppContext.java
+++ src/de/reneruck/connisRezepteApp/AppContext.java
@@ -19,8 +19,15 @@ public class AppContext extends Application {
 		SQLiteDatabase db= this.manager.getReadableDatabase();
 		Cursor c = db.query(Configurations.table_Rezepte, new String[]{"*"}, Configurations.rezepte_Id + "=" + documentId, null, null, null, null);
 		String query = "select * from " + Configurations.table_Rezept_to_Zutat + 
-				" JOIN " + Configurations.rezept_to_zutat_rezeptId + " ON (" + Configurations.table_Rezepte + "." + Configurations.rezepte_Id + ")" +
-				"  where " + Configurations.rezept_to_zutat_rezeptId + " = " + documentId;
+				" INNER JOIN " + Configurations.table_Rezepte + 
+				" ON (" + Configurations.table_Rezepte+ "." + Configurations.rezepte_Id + 
+						" = "+ Configurations.table_Rezept_to_Zutat + "." + Configurations.rezept_to_zutat_rezeptId+ ")" +
+				" where " + Configurations.rezept_to_zutat_rezeptId + " = " + documentId;
+		Cursor zutaten = db.rawQuery(query, null);
+		/*
+		 * select Zutaten.value from Zutaten, Rezept_Zutat, Rezepte JOIN Rezepte ON (Rezepte.ID = Rezept_Zutat.rezeptId)  JOIN Rezept_Zutat ON (Rezept_Zutat.zutatId = Zutat.ID) where Rezepte.ID = 1891913874
+		 */
+		c.moveToFirst();
 		if(c.getCount() > 0){
 			c.moveToFirst();
 			Rezept rezept = new Rezept(c);
