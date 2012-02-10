b4760e84b023fc83ace43f4d1ac8fdfd9637b2a6
Rene <reneruck@googlemail.com>
2012-02-10 22:20:09 +0100

    FIX/NEW fixed database export and created a database import

--- src/de/reneruck/connisRezepteApp/development/DatabaseOverview.java
+++ src/de/reneruck/connisRezepteApp/development/DatabaseOverview.java
@@ -5,20 +5,24 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-
-import org.apache.http.util.ByteArrayBuffer;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
 
 import android.app.ActionBar;
 import android.app.ActionBar.Tab;
 import android.app.ActionBar.TabListener;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.FragmentTransaction;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.util.Log;
+import android.text.format.DateFormat;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -26,15 +30,18 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TableLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 import de.reneruck.connisRezepteApp.AppContext;
 import de.reneruck.connisRezepteApp.Configurations;
 import de.reneruck.connisRezepteApp.Main;
 import de.reneruck.connisRezepteApp.R;
+import de.reneruck.connisRezepteApp.Utils;
 import de.reneruck.connisRezepteApp.DB.DBManager;
 
 public class DatabaseOverview extends Activity implements TabListener {
 
 	private DBManager dbManager;
+    private String BACKUP_DIR = "/sdcard/ConnisRezepteApp/";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -66,6 +73,33 @@ public class DatabaseOverview extends Activity implements TabListener {
 	    return true;
 	}
 
+	private void importDBFile(String string) {
+		final String input = string;
+		
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setMessage(R.string.import_database)
+		       .setCancelable(false)
+		       .setPositiveButton("Ja", new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog, int id) {
+		        	   String trim = input.split(" - ")[1].replace("(", " ").replace(")", " ").trim();
+		        	   if(trim.endsWith(".backup")) trim = trim.replace(".backup", " ").trim();
+		        	   File backupFile = new File(BACKUP_DIR + trim);
+		        	   if(backupFile.exists() && backupFile.getName().endsWith(".db") && !backupFile.isDirectory()){
+		        		   File dbfile_toRemove = new File("/data/data/de.reneruck.connisRezepteApp/databases/rezepte.db");
+		        		   dbfile_toRemove.delete();
+		        		   Utils.copyFile(backupFile, dbfile_toRemove);
+		        	   }
+		                
+		           }
+		       })
+		       .setNegativeButton("Nein", new DialogInterface.OnClickListener() {
+		           public void onClick(DialogInterface dialog, int id) {
+		                dialog.cancel();
+		           }
+		       })
+		       .create().show();		
+	}
+	
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 	    switch (item.getItemId()) {
@@ -77,31 +111,70 @@ public class DatabaseOverview extends Activity implements TabListener {
 	            
 	    	case R.id.menu_debug_copydb:
 	    		File dbfile = new File("/data/data/de.reneruck.connisRezepteApp/databases/rezepte.db");
-	    		File copiedFile = new File("/sdcard/rezepte.db");
-	    		if (copiedFile.exists()) {
-					copiedFile = new File("/sdcard/rezepte-"+System.currentTimeMillis()+".db");
-				}
-				try {
-					FileInputStream fileInputStream = new FileInputStream(dbfile);
-					FileOutputStream fileOutputStream = new FileOutputStream(copiedFile);
-					byte[] buffer = new ByteArrayBuffer((int) dbfile.length()).toByteArray();
-					fileInputStream.read(buffer);
-					fileOutputStream.write(buffer);
-					fileOutputStream.flush();
-					fileInputStream.close();
-					fileOutputStream.close();
-				} catch (FileNotFoundException e1) {
-					e1.printStackTrace();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
+	    		File copiedFile = new File(BACKUP_DIR + "rezepte-"+System.currentTimeMillis()+".db");
+	        	File backupDir = new File(BACKUP_DIR);
+	        	if(!backupDir.exists()) backupDir.mkdirs(); 
+	    		Utils.copyFile(dbfile, copiedFile);
+					
 	            return true;
 	            
+	    	case R.id.menu_debug_importdb:
+	    		
+	    		File importFolder = new File(BACKUP_DIR);
+	    		boolean noFiles = false;
+	    		if(importFolder.isDirectory()){
+	    			String[] files = importFolder.list();
+	    			final List<String> items = new LinkedList<String>();
+	    			for (int i = 0; i < files.length; i++) {
+						if(files[i].matches("rezepte-(\\d+).db") || files[i].matches("rezepte-(\\d+).db.backup")){
+							String replace = files[i].replace("rezepte-", " ").replace(".db", " ").trim();
+				        	   if(replace.endsWith(".backup")) replace = replace.replace(".backup", " ").trim();
+							items.add((String) DateFormat.format("dd.MM.yyyy hh:mm", new Date(Long.decode(replace))) 
+									+ " - (" + files[i] + ") ");
+						}
+					}
+	    			
+	    			if(items.size() == 0) {
+	    				items.add("Keine Exportierten Datenbanken gefunden");
+	    				noFiles = true;
+	    			}
+	    			AlertDialog.Builder builder = new AlertDialog.Builder(this);
+	    			builder.setTitle("Exportierte Datenbank Importieren");
+	    			if(noFiles) {
+	    				builder.setItems(Arrays.copyOf(items.toArray(), items.size(), String[].class), new DialogInterface.OnClickListener() {
+	    					public void onClick(DialogInterface dialog, int item) {
+	    					}
+	    				});
+	    				
+	    			} else {
+	    				builder.setItems(Arrays.copyOf(items.toArray(), items.size(), String[].class), new DialogInterface.OnClickListener() {
+	    					public void onClick(DialogInterface dialog, int item) {
+	    						importDBFile(items.get(item));
+	    					}
+	    				});
+	    			}
+	    			builder.create().show();
+	    		}
+
+	    		return true;
+	    		
 	        case R.id.menu_debug_cleandb:
 	        	try {
-	        		SQLiteDatabase db = this.dbManager.getWritableDatabase();
-	        		db.delete(Configurations.table_Rezepte, null, null);
-	        		db.close();
+	        		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+	        		builder.setMessage("Are you sure you want to clear the Database?")
+	        		       .setCancelable(false)
+	        		       .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+	        		           public void onClick(DialogInterface dialog, int id) {
+	        		                File dbfile_toRemove = new File("/data/data/de.reneruck.connisRezepteApp/databases/rezepte.db");
+	        		                dbfile_toRemove.delete();
+	        		           }
+	        		       })
+	        		       .setNegativeButton("No", new DialogInterface.OnClickListener() {
+	        		           public void onClick(DialogInterface dialog, int id) {
+	        		                dialog.cancel();
+	        		           }
+	        		       })
+	        		       .create().show();
 	        		
 				} catch (SQLException e) {
 						e.printStackTrace();
@@ -111,13 +184,25 @@ public class DatabaseOverview extends Activity implements TabListener {
 	            return super.onOptionsItemSelected(item);
 	    }
 	}
-	
+
 	@Override
 	public void onTabReselected(Tab tab, FragmentTransaction ft) {
 		// TODO Auto-generated method stub
 		
 	}
 
+	@Override
+	public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+
+		if (tab.getText().toString().equals("Andere Tabellen")) {
+			
+		} else {
+			
+		}
+
+		
+	}
+	
 	@Override
 	public void onTabSelected(Tab tab, FragmentTransaction ft) {
 //		((ViewGroup)findViewById(R.id.debug_scroll)).removeAllViews();
@@ -129,7 +214,7 @@ public class DatabaseOverview extends Activity implements TabListener {
 			show3parted(Configurations.table_Rezept_to_Kategorie, Configurations.rezept_to_kategorie_kategorieId, R.id.debug_table_rezept_to_kategorien);
 			show3parted(Configurations.table_Rezept_to_Zutat, Configurations.rezept_to_zutat_zutatId, R.id.debug_table_rezept_to_zutat);
 			
-		}else if(tab.getText().toString().equals("Rezepte")) {
+		} else if(tab.getText().toString().equals("Rezepte")) {
 			showRezepte();
 		}
 	}
@@ -232,11 +317,5 @@ public class DatabaseOverview extends Activity implements TabListener {
 			}
 		}
 		db.close();		
-	}
-
-	@Override
-	public void onTabUnselected(Tab tab, FragmentTransaction ft) {
-		// TODO Auto-generated method stub
-		
 	}
 }
